7857d4bffb450b5380db93a413ac0947db541584
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-07-14 18:08:16 -0500

    In this commit, I updated some styles for when the user selects the option to mark a todo as complete

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -43,7 +43,7 @@ function TodoItem(props) {
   }, [editMode]);
 
   return (
-    <li className="TodoItem">
+    <li className={`TodoItem ${props.completed && 'TodoItem--complete'}`}>
       <CompleteIcon completed={props.completed} onComplete={props.onComplete} />
       <p
         className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}
